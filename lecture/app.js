0297c6d67af33ca771cd67a2d0fd7419e3fde253
1309042 <1309042@naver.com>
2023-04-07 22:59:04 +0900

    회원가입
    회원가입 kakao 모듈

--- lecture/app.js
+++ lecture/app.js
@@ -1,67 +1,72 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
-const path = require('path')
-const session = require('express-session')
-const nunjucks = require('nunjucks') // 풀스택 개발자를 생각하면 react, vue로 만들어보기
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 const passport = require('passport');
-const { sequelize } = require('./models');
-// process.env.COOKIE_SECRET 없음
-dotenv.config(); // process.env
-// process.env.COOKIE_SECRET 있음
+
+dotenv.config();
 const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
+const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
 const app = express();
-passportConfig();
-app.set('port', process.env.Port || 8001);
+passportConfig(); // 패스포트 설정
+app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
-    express: app,
-    watch: true,
+  express: app,
+  watch: true,
 });
-sequelize.sync({ force: false }) // 개발 시에만 쓰기 (잘못 만들었을 경우 대비)
-    .then(() => {
-        console.log('데이터베이스 연결 성공')
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-    
-app.use(morgan('dev')); // 개발모드 // 배포할 땐 combined
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json()); // req.body를 ajax json 요청으로부터
-app.use(express.urlencoded({ extended: false })); // req.body 폼으로부터
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    }
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 }));
-app.use(passport.initialize()); // req.user, req.login, req.isAuthenticate, req.logout
-app.use(passport.session()); // connect.id 라는 이름으로 세션 쿠키가 브라우저로 전송
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
+app.use('/post', postRouter);
+app.use('/user', userRouter);
 
-app.use((req, res, next) => { // 404 NOT FOUND
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== 'production' ? err: {}; // 에러 로그를 서비스한테 넘겨요
-    res.status(err.status || 500);
-    res.render('error');
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기 중');
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
--- lecture/controllers/auth.js
+++ lecture/controllers/auth.js
@@ -1,5 +1,24 @@
-exports.join = () => {
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
+exports.join = async (req, res, next) => {
+    const { nick, email, password } = req.body;
+    try {
+        const exUser = await User.findOne({ where: { email }});
+        if (exUser) {
+            return res.redirect('/join?error=exist');
+        }
+        const hash = await bcrypt.hash(password, 12);
+        await User.create({
+            email,
+            nickn,
+            password: hash,
+        });
+        return res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        next(error)
+    }
 }
 
 exports.login = () => {
